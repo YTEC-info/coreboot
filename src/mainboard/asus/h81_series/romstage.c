f29b6a03c89454a6f5a791586ac97e089fa74208
YTEC-info <73857225+YTEC-info@users.noreply.github.com>
2024-10-05 00:45:51 -0300

    Update romstage.c

--- src/mainboard/asus/h81_series/romstage.c
+++ src/mainboard/asus/h81_series/romstage.c
@@ -2,15 +2,17 @@
 
 #include <stdint.h>
 #include <northbridge/intel/haswell/haswell.h>
+#include <northbridge/intel/haswell/raminit.h>
 #include <southbridge/intel/lynxpoint/pch.h>
 
 void mainboard_config_rcba(void)
 {
 }
 
-/* FIXME: called after romstage_common, remove it if not used */
-void mb_late_romstage_setup(void)
+void mb_get_spd_map(struct spd_info *spdi)
 {
+	spdi->addresses[0] = 0x50;
+	spdi->addresses[2] = 0x52;
 }
 
 const struct usb2_port_config mainboard_usb2_ports[MAX_USB2_PORTS] = {
