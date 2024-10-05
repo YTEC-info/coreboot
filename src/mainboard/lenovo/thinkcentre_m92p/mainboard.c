0967c34546c5901653b811e79d098f9fd0a0e665
YTEC-info <73857225+YTEC-info@users.noreply.github.com>
2024-10-04 22:45:08 -0300

    Update mainboard.c
    Add beep

--- src/mainboard/lenovo/thinkcentre_m92p/mainboard.c
+++ src/mainboard/lenovo/thinkcentre_m92p/mainboard.c
@@ -1,16 +1,19 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
-
 #include <device/device.h>
 #include <drivers/intel/gma/int15.h>
-
+#include <southbridge/intel/bd82x6x/pch.h>
+#include <pc80/i8254.h>
+static void mainboard_final(void *unused)
+{
+        beep(2500, 100);
+}
 static void mainboard_enable(struct device *dev)
 {
-	/* FIXME: fix these values. */
-	install_intel_vga_int15_handler(GMA_INT15_ACTIVE_LFP_INT_LVDS,
+	install_intel_vga_int15_handler(GMA_INT15_ACTIVE_LFP_NONE,
 					GMA_INT15_PANEL_FIT_DEFAULT,
 					GMA_INT15_BOOT_DISPLAY_DEFAULT, 0);
 }
-
 struct chip_operations mainboard_ops = {
 	.enable_dev = mainboard_enable,
+	.final = mainboard_final,
 };
